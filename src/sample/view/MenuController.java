c4655ac2d21655b1af7c164b08981c94e07ee84e
Kevin O <Kevinnuricumbo@gmail.com>
2021-08-01 03:51:48 -0500

    Reporte generado.

--- src/sample/view/MenuController.java
+++ src/sample/view/MenuController.java
@@ -16,8 +16,10 @@ import sample.Tablas.CuentasCobrar;
 import sample.model.Categoria;
 import sample.model.Conexion;
 import sample.model.FlujoEfectivo;
+import sample.model.Semana;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -62,7 +64,7 @@ public class MenuController {
     private AnchorPane GenerarReporteAnchor;
 
     @FXML
-    private ComboBox<?> comboMeses;
+    private ComboBox<String> comboMeses;
 
     @FXML
     private AnchorPane windowindicadorsub;
@@ -147,6 +149,7 @@ public class MenuController {
     String selecioncombo = "";
     String selectcombocategoria ="";
     ObservableList<Categoria> itemcategoriacombo = FXCollections.observableArrayList();
+    String meses [] = {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
 
     Boolean auxwindowcombo = false;
     public void openWindowCategoria(){
@@ -266,7 +269,13 @@ public class MenuController {
     }
 
     public void openGenerarReporte(){
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (int i=0; i<12; i++){
+            items.add(meses[i]);
+        }
+        comboMeses.setItems(items);
         GenerarReporteAnchor.setVisible(true);
+
     }
     public void cerrarGenerarReporte(){
         GenerarReporteAnchor.setVisible(false);
@@ -274,9 +283,16 @@ public class MenuController {
     @FXML private AnchorPane tablaReporte;
 
     public void openGenerarReporteTabla(){
-        GenerarReporteAnchor.setVisible(false);
-        llenarTabla();
-        tablaReporte.setVisible(true);
+        if(comboMeses.getSelectionModel().getSelectedItem() != null){
+            GenerarReporteAnchor.setVisible(false);
+            llenarTabla();
+            tablaReporte.setVisible(true);
+        }else {
+            Alert advertencia = new Alert(Alert.AlertType.WARNING);
+            advertencia.setTitle("Error");
+            advertencia.setHeaderText("Selecciones un mes");
+            advertencia.show();
+        }
 
     }
     public void cerrarGenerarReporteTabla(){
@@ -326,7 +342,7 @@ public class MenuController {
 
     @FXML private TableColumn colFinalI;
 
-    @FXML private TableView<CuentasCobrar> tablaCuentaGastos;
+    @FXML private TableView<Semana> tablaCuentaGastos;
 
     @FXML private TableColumn colNombreG;
 
@@ -366,6 +382,11 @@ public class MenuController {
 
 
     public void llenarTabla(){
+        System.out.println(comboMeses.getSelectionModel(). getSelectedItem());
+        Conexion conexion = new Conexion();
+        ObservableList <Semana> listaReporte = conexion.generarPdf();
+        String mes = comboMeses.getSelectionModel(). getSelectedItem();
+
         cuentasCobrar = FXCollections.observableArrayList();
         llenaDatosCeuntasPorCobrar();
         this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
@@ -385,7 +406,7 @@ public class MenuController {
         this.colFinalP.setCellValueFactory(new PropertyValueFactory("Final"));
 
         cuentasIngresos = FXCollections.observableArrayList();
-        llenaDatosIngresos();
+        llenaDatosIngresos(listaReporte, mes);
         this.colNombreI.setCellValueFactory(new PropertyValueFactory("nombre"));
         this.sem1I.setCellValueFactory(new PropertyValueFactory("semana1"));
         this.sem2I.setCellValueFactory(new PropertyValueFactory("semana2"));
@@ -429,19 +450,80 @@ public class MenuController {
         }
     }
 
-    public void llenaDatosIngresos(){
-        for(int i=0; i<5; i++){
-            CuentasCobrar cuentasI = new CuentasCobrar("Efectivo "+i,"9000","3500","0","1200","1200");
-            cuentasIngresos.add(cuentasI);
-            tablaCuentaIngreso.setItems(cuentasIngresos);
+    public void llenaDatosIngresos(ObservableList<Semana> lista, String mes){
+        Boolean axu = false;
+        ArrayList <Semana> listaEntrada = new ArrayList<>();
+        for (int i=0; i<lista.size(); i++){
+            if(lista.get(i).getTipo().equals("entrada") && lista.get(i).getMes().equals(mes)) {
+                listaEntrada.add(lista.get(i));
+                axu = true;
+                System.out.println(lista.get(i).getDescripcion() + " descripcion:semana " + lista.get(i).getNumeroSemana());
+            }
         }
+
+        if(axu){
+            Double monto1 = 0.0, monto2 =0.0, monto3 =0.0, monto4 =0.0, monto5 =0.0;
+            ArrayList <String> auxList = new ArrayList<>();
+            for (int i=0; i<= listaEntrada.size(); i++){
+                String sema1 = "", sema2 = "", sema3 = "", sema4 = "", sema5 = "", nameAux;
+                int auxNum=0;
+                Boolean aux2 = false;
+                if( i< listaEntrada.size()){
+                    nameAux = listaEntrada.get(i).getDescripcion();
+                    for (int j=0; j<listaEntrada.size(); j++){
+                        if(listaEntrada.get(j).getDescripcion().equals(nameAux)){
+                            switch (listaEntrada.get(j).getNumeroSemana()){
+                                case 1: sema1 = String.valueOf(listaEntrada.get(j).getMonto()); break;
+                                case 2: sema2 = String.valueOf(listaEntrada.get(j).getMonto()); break;
+                                case 3: sema3 = String.valueOf(listaEntrada.get(j).getMonto()); break;
+                                case 4: sema4 = String.valueOf(listaEntrada.get(j).getMonto()); break;
+                                case 5: sema5 = String.valueOf(listaEntrada.get(j).getMonto()); break;
+                            }
+                            auxNum = auxNum + 1;
+                        }
+                    }
+                    switch (listaEntrada.get(i).getNumeroSemana()){
+                        case 1: monto1 = monto1 + listaEntrada.get(i).getMonto(); break;
+                        case 2: monto2 = monto2 + listaEntrada.get(i).getMonto(); break;
+                        case 3: monto3 = monto3 + listaEntrada.get(i).getMonto(); break;
+                        case 4: monto4 = monto4 + listaEntrada.get(i).getMonto(); break;
+                        case 5: monto5 = monto5 + listaEntrada.get(i).getMonto(); break;
+                    }
+                    for (int h=0; h<auxList.size(); h++){
+                        System.out.println(auxList.get(h));
+                        if (nameAux.equals(auxList.get(h))){
+                            System.out.println("Entro:" + auxList.size());
+                            aux2 = true;
+                        }
+                    }
+                    if(!aux2){
+                        System.out.println("Hola: " + aux2);
+                        CuentasCobrar cuentasI = new CuentasCobrar(nameAux, sema1, sema2, sema3, sema4, sema5);
+                        cuentasIngresos.add(cuentasI);
+                        tablaCuentaIngreso.setItems(cuentasIngresos);
+                        auxList.add(nameAux);
+                    }
+                } else {
+                    CuentasCobrar cuentasI = new CuentasCobrar("Total: ", String.valueOf(monto1),  String.valueOf(monto2),  String.valueOf(monto3),  String.valueOf(monto4),  String.valueOf(monto5));
+                    cuentasIngresos.add(cuentasI);
+                    tablaCuentaIngreso.setItems(cuentasIngresos);
+                    System.out.println("ultima fila");
+                }
+            }
+        } else {
+            Alert advertencia = new Alert(Alert.AlertType.WARNING);
+            advertencia.setTitle("Sin registros");
+            advertencia.setHeaderText("No hay registros del mes de: " + mes);
+            advertencia.show();
+        }
+
     }
 
     public void llenaDatosGastos(){
         for(int i=0; i<5; i++){
             CuentasCobrar cuentasG = new CuentasCobrar("Operativos "+i,"9000","3500","0","1200","1200");
             cuentasGastos.add(cuentasG);
-            tablaCuentaGastos.setItems(cuentasGastos);
+            //tablaCuentaGastos.setItems(cuentasGastos);
         }
     }
 
